59065233df63e6c88fc2deca55568f856522c759
EijiShitara <c181408m@alm.icu.ac.jp>
2025-03-01 21:28:38 +0900

    first commit

---
+++ src/main.lisp
@@ -0,0 +1,14 @@
+(defparameter *unify-global-counter* 10)
+
+(defun unify-dg (dg1 dg2)
+  )
+
+(defun unify-dg0 (dg1 dg2)
+  )
+
+(defun unify-dg1 (dg1-underef dg2-underef2)
+  )
+
+;; Graph node dereferencing
+(defun dereference-dg (dg)
+  )
